c10c3eb26820cd9a828318523890871e0a9d069c
Bairy <jrosalinas@gmail.com>
2017-04-18 23:06:13 +0800

    Currently having effing errors

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -24,7 +24,7 @@
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-
+    @yield('styles')
 
 
 </head>
@@ -77,25 +77,25 @@
 
 
 
-        {{--<ul class="nav navbar-nav navbar-right">--}}
-        {{--@if(auth()->guest())--}}
-        {{--@if(!Request::is('auth/login'))--}}
-        {{--<li><a href="{{ url('/auth/login') }}">Login</a></li>--}}
-        {{--@endif--}}
-        {{--@if(!Request::is('auth/register'))--}}
-        {{--<li><a href="{{ url('/auth/register') }}">Register</a></li>--}}
-        {{--@endif--}}
-        {{--@else--}}
-        {{--<li class="dropdown">--}}
-        {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ auth()->user()->name }} <span class="caret"></span></a>--}}
-        {{--<ul class="dropdown-menu" role="menu">--}}
-        {{--<li><a href="{{ url('/auth/logout') }}">Logout</a></li>--}}
+        <ul class="nav navbar-nav navbar-right">
+        @if(auth()->guest())
+        @if(!Request::is('auth/login'))
+        <li><a href="{{ url('/auth/login') }}">Login</a></li>
+        @endif
+        @if(!Request::is('auth/register'))
+        <li><a href="{{ url('/auth/register') }}">Register</a></li>
+        @endif
+        @else
+        <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ auth()->user()->name }} <span class="caret"></span></a>
+        <ul class="dropdown-menu" role="menu">
+        <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
 
-        {{--<li><a href="{{ url('/admin/profile') }}/{{auth()->user()->id}}">Profile</a></li>--}}
-        {{--</ul>--}}
-        {{--</li>--}}
-        {{--@endif--}}
-        {{--</ul>--}}
+        <li><a href="{{ url('/admin/profile') }}/{{auth()->user()->id}}">Profile</a></li>
+        </ul>
+        </li>
+        @endif
+        </ul>
 
 
 
@@ -127,7 +127,7 @@
                             </li>
 
                             <li>
-                                <a href="/users/create">Create User</a>
+                                <a href="/git add .users/create">Create User</a>
                             </li>
 
                         </ul>
