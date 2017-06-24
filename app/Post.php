c8c003ee52707d7cee4208ca15120e2ed0783b8f
Bairy <jrosalinas@gmail.com>
2017-06-24 20:05:39 +0800

    able to create post with statuc categeory

---
+++ app/Post.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //
+    protected $fillable = [
+
+        'body',
+        'photo_id',
+        'category_id',
+        'title'
+        ];
+
+    public function user(){
+
+        return $this->belongsTo('App\User');
+
+    }
+
+    public function photo(){
+
+        return $this->belongsTo('App\Photo');
+
+    }
+
+    public function category(){
+
+        return $this->belongsTo('App\Category');
+
+    }
+}
--- app/User.php
+++ app/User.php
@@ -33,4 +33,34 @@ class User extends Authenticatable
     public function photo(){
         return $this->belongsTo('App\Photo');
     }
+
+    public function setPasswordAttribute($password){
+
+        if(!empty($password)){
+
+            $this->attributes['passsword']= bcrypt($password);
+
+        }
+
+    }
+
+
+    public function isAdmin(){
+
+        if($this->role->name == "administrator" &&
+        $this->is_active == 1){
+
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+    public function posts(){
+
+        return $this->hasMany('App\Post');
+
+    }
 }
